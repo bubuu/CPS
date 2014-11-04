d07dbbb54d4c9ca83ed935e4624923f36b985ae8
Malgorzata Targan <malgorzata.targan@gmail.com>
2014-11-04 16:43:25 +0100

    zad 2 - change titles and repair

--- lab2_zad2.m
+++ lab2_zad2.m
@@ -7,18 +7,55 @@ close all
 N2 = 15;
 n = [0: N2-1];
 
-K = 3;
-w0 = 2*pi;
 Ts = 1
 t = 0:Ts:16
+Tsr = 16/(4*length(t)-1)
+t2 = 0:Tsr:16
+
 Fs = 1
 xt = exp(-t/4).*sin(2*pi*t/4);
-xn = exp(-n/Fs/4).*sin(2*pi*n/Fs/4);
-X = fft(xn,N2);
+figure
+subplot(2,1,1);
+plot(t,xt,'.'); 
+xlabel('t [s]'); 
+ylabel('x(t)'); 
+title('Pierwotny');
 
-Y=[X(1:(N2+1)/2), zeros(1,K*N2), X((N2+1)/2+1:N2)]; 
-xr = (K+1)*ifft(Y); 
-figure
-plot(t, xt,'o')
-figure
-plot(xr)
+y = reconst(xt, t2);
+
+subplot(2,1,2);
+plot(t2,y,'.'); 
+hold all %dla porownania
+t=0:Ts:16;
+plot(t,xt,'or'); 
+xlabel('t [s]'); 
+ylabel('resamplowany x(t)'); 
+title('Resamplowany')
+
+
+% clc;
+% clear all
+% close all;
+% 
+% T=1;
+% tk=16;
+% t=0:T:tk;
+% x=exp(-t/4).*sin(2*pi*t/5);
+% figure
+% subplot(2,1,1);
+% plot(t,x,'.'); 
+% xlabel('t [s]'); 
+% ylabel('x(t)'); 
+% title('Pierwotny');
+% 
+% xres=resamp(x,4*length(x));
+% subplot(2,1,2);
+% p=16/(length(xres)-1);
+% t=0:p:16;
+% plot(t,xres,'.'); 
+% %hold all %dla porownania
+% %t=0:T:tk;
+% %plot(t,x,'or'); 
+% xlabel('t [s]'); 
+% ylabel('resamplowany x(t)'); 
+% title('Resamplowany')
