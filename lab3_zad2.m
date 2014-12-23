657c7491bc5619dd2f9ad6466ada6cab34a8d003
Malgorzata Targan <malgorzata.targan@gmail.com>
2014-12-23 13:37:57 +0100

    lab 3 zad 2

--- lab3_zad2.m
+++ lab3_zad2.m
@@ -9,11 +9,14 @@ Udc = 3;
 Urms = 2;
 
 %select apropriate range
-x = [-2:0.1:8]
+x = [-2:0.1:8];
 
 %calculate 
-f = 1/sqrt(2*pi*Urms)*exp(-((x-Udc).^2)/(2*Urms))
+f = 1/sqrt(2*pi*Urms)*exp(-((x-Udc).^2)/(2*Urms));
 
 %show
 plot(x,f);
+title('Funkcja gestosci prawdopodobienstwa szumu')
+ylabel('p(x)')
+xlabel('x')
 
