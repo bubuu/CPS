c4faeb52797a6847ab591934a414f4042ee2af7e
Malgorzata Targan <malgorzata.targan@gmail.com>
2014-12-30 00:07:21 +0100

    lab 3 zad 3

--- lab2_zad3.m
+++ lab2_zad3.m
@@ -1,26 +1,37 @@
+%Malgorzata Targan
+% 131420
+% lab 2
+
 clear all
 close all
 
 T = [0.04 0.07];
-N=64;
-n = -N/2:(N/2-1);
+Tmax = max(T);
+Fmax = 40;
+N = ceil(Tmax*Fmax);
+n = 0:N-1;
 m(1,:) = n/T(1);
 m(2,:) = n/T(2);
-f= 1:1:40;
-%m = [0:M];
-%m = 0:M
-F = 40;
+f= 0:0.1:Fmax;
+
+
 X = zeros(2,length(f));
 
 for i=1:length(f)
-    X(1,i)=1/T(1) * sum(10*exp((-abs(f(i) - m(1,:)))/2));
-    X(2,i)=1/T(2) * sum(10*exp((-abs(f(i) - m(2,:)))/2)); 
+    X(1,i)=1/T(1)*sum(10*exp((-abs(f(i)-m(1,:)))/2));
+    X(2,i)=1/T(2)*sum(10*exp((-abs(f(i)-m(2,:)))/2)); 
 end
-%X(:,1) = X(:,1) + 10*exp(-1*abs(f-m/T(2))/2);
-subplot(2,1,1)
-plot(f,abs(X(1,:)),'-x'); 
-title('przedzial probkowania = 0.04 s'); 
-xlabel('Czestotliwosc [Hz]'); 
+
+subplot(3,1,1)
+plot(f,10*exp(-abs(f)/2));
+subplot(3,1,2)
+plot(f,abs(X(1,:))); 
+title('T = 0.04s'); 
+xlabel('f[Hz]'); 
 ylabel(['|X(j\omega)|']) 
-% subplot(2,1,2)
-% plot(X(:,2))
+subplot(3,1,3)
+plot(X(:,2))
+plot(f,abs(X(2,:))); 
+title('T = 0.07s'); 
+xlabel('f[Hz]'); 
+ylabel(['|X(j\omega)|']) 
